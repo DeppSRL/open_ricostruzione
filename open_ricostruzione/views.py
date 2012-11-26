b1f6dd851b24983dad1e1d0b47a529061a51dbaa
verganis <stefano.vergani.it@gmail.com>
2012-11-26 11:22:32 +0100

    Data extraction for comune ok

--- open_ricostruzione/views.py
+++ open_ricostruzione/views.py
@@ -29,7 +29,37 @@ class ComuneView(DetailView):
     template_name = 'comune.html'
 
     def get_context_data(self, **kwargs ):
-        c = self.object()
+        c = self.get_object()
         context = super(ComuneView, self).get_context_data(**kwargs)
+
+        # donazioni per il comune considerato
+        tot_donazioni = Donazione.objects.filter(comune=c).aggregate(s=Sum('importo')).values()
+        if tot_donazioni:
+            context['tot_donazioni']= tot_donazioni
+            context['donazioni'] = Donazione.objects.filter(comune=c)
+
+        # importi dei progetti per categorie
+        context['progetti_categorie'] =  \
+            Progetto.objects.filter(comune=c).values('tipologia').\
+            annotate(somma_categoria=Sum('riepilogo_importi'))
+
+        # donazioni divise per tipologia cedente
+        context['donazioni_categorie'] = \
+            Donazione.objects.filter( comune=c).\
+            filter(confermato = True).values('tipologia').\
+            annotate(somma_categoria = Sum('importo'))
+
+        #iban comune
+        iban = Comune.objects.get(pk = c.pk).iban
+        if iban:
+            context['iban'] = iban
+
+        #lista progetti per questo comune in ordine di costo decrescente
+        projects = Progetto.objects.filter(comune = c).order_by('-riepilogo_importi')[:10]
+
+        if projects:
+            context['projects'] = projects
+
+
 
         return context
