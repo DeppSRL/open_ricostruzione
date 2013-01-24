f9271ffe8d0e3d29a4aca982e6c7623ec9872a44
verganis <stefano.vergani.it@gmail.com>
2013-01-24 13:18:58 +0100

    Added django-sociable

--- open_ricostruzione/settings.py
+++ open_ricostruzione/settings.py
@@ -54,7 +54,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -136,6 +136,7 @@ INSTALLED_APPS = (
     'django_extensions',
     'open_ricostruzione.depp_humanize',
     'disqus',
+    'sociable',
 )
 
 
