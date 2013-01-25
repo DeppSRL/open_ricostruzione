b891a100391717aa54ad29d59d89ffb6fe1a0ad5
verganis <stefano.vergani.it@gmail.com>
2013-01-25 18:10:18 +0100

    New boxes for Danno and Donazioni Home page OK

--- open_ricostruzione/depp_humanize/templatetags/italianize.py
+++ open_ricostruzione/depp_humanize/templatetags/italianize.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
+'''
+this template filter transforms cypher numbers in the Italian format for money:
+ ex. 91.234.333,22 (for Euro)
+'''
+
+
+
 from django import template
 from open_ricostruzione.utils.moneydate import moneyfmt
 
