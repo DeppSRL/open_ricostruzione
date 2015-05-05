616e6ebcdaac7873fa9d78b2623a781571e40eeb
verganis <stefano.vergani.it@gmail.com>
2015-05-05 11:27:14 +0200

    Commented part of chart-venn

--- open_ricostruzione/static/js/charts/chart-venn.js
+++ open_ricostruzione/static/js/charts/chart-venn.js
@@ -15,7 +15,7 @@ $(function(){
       tooltip: { enabled: false },
       plotOptions: {
           series: {
-            showInLegend: false,
+            showInLegend: false
             //point: {
             //  events: {
             //    mouseOver: function(){
@@ -44,51 +44,51 @@ $(function(){
     //return false;
 
 
+//
+//
+//
+//    var progr1  = parseFloat($(".soggetti-attuatori-1 .importo-soggetti-programmati").html());
+//    var pianif1 = parseFloat($(".soggetti-attuatori-1 .importo-soggetti-pianificati").html());
+//    var attual1 = parseFloat($(".soggetti-attuatori-1 .importo-soggetti-attuati").html());
 
-
-
-    var progr1  = parseFloat($(".soggetti-attuatori-1 .importo-soggetti-programmati").html());
-    var pianif1 = parseFloat($(".soggetti-attuatori-1 .importo-soggetti-pianificati").html());
-    var attual1 = parseFloat($(".soggetti-attuatori-1 .importo-soggetti-attuati").html());
-
-    $('#chart-venn-soggetti1').highcharts({
-      chart:{
-        backgroundColor: "rgba(255, 255, 255, 0)",
-        margin: 0,
-        padding: 0
-      },
-      title: {
-          text: ''
-      },
-      tooltip: { enabled: false },
-      plotOptions: {
-          series: {
-            showInLegend: false,
-            //point: {
-            //  events: {
-            //    mouseOver: function(){
-            //      this.graphic.element.setAttribute('stroke-width', 2);
-            //      this.graphic.element.setAttribute('stroke', '#00f');
-            //    },
-            //    mouseOut: function () {
-            //      this.graphic.element.setAttribute('stroke-width', 0);
-            //    }
-            //  }
-            //}
-          }
-      },
-      series: [{
-          type: 'venn',
-          data: [
-              ['A', + attual1],
-              ['B', + pianif1],
-              ['C', + progr1],
-              ['A - B', + attual1],
-              ['B - C', + pianif1],
-              ['A - C', + 0]
-          ]
-      }]
-    });
+//    $('#chart-venn-soggetti1').highcharts({
+//      chart:{
+//        backgroundColor: "rgba(255, 255, 255, 0)",
+//        margin: 0,
+//        padding: 0
+//      },
+//      title: {
+//          text: ''
+//      },
+//      tooltip: { enabled: false },
+//      plotOptions: {
+//          series: {
+//            showInLegend: false,
+//            //point: {
+//            //  events: {
+//            //    mouseOver: function(){
+//            //      this.graphic.element.setAttribute('stroke-width', 2);
+//            //      this.graphic.element.setAttribute('stroke', '#00f');
+//            //    },
+//            //    mouseOut: function () {
+//            //      this.graphic.element.setAttribute('stroke-width', 0);
+//            //    }
+//            //  }
+//            //}
+//          }
+//      },
+//      series: [{
+//          type: 'venn',
+//          data: [
+//              ['A', + attual1],
+//              ['B', + pianif1],
+//              ['C', + progr1],
+//              ['A - B', + attual1],
+//              ['B - C', + pianif1],
+//              ['A - C', + 0]
+//          ]
+//      }]
+//    });
     //return false;
     
     
@@ -96,98 +96,98 @@ $(function(){
     
     
     
-    var progr2  = parseFloat($(".soggetti-attuatori-2 .importo-soggetti-programmati").html());
-    var pianif2 = parseFloat($(".soggetti-attuatori-2 .importo-soggetti-pianificati").html());
-    var attual2 = parseFloat($(".soggetti-attuatori-2 .importo-soggetti-attuati").html());
-
-    $('#chart-venn-soggetti2').highcharts({
-      chart:{
-        backgroundColor: "rgba(255, 255, 255, 0)",
-        margin: 0,
-        padding: 0
-      },
-      title: {
-          text: ''
-      },
-      tooltip: { enabled: false },
-      plotOptions: {
-          series: {
-            showInLegend: false,
-            //point: {
-            //  events: {
-            //    mouseOver: function(){
-            //      this.graphic.element.setAttribute('stroke-width', 2);
-            //      this.graphic.element.setAttribute('stroke', '#00f');
-            //    },
-            //    mouseOut: function () {
-            //      this.graphic.element.setAttribute('stroke-width', 0);
-            //    }
-            //  }
-            //}
-          }
-      },
-      series: [{
-          type: 'venn',
-          data: [
-              ['A', + attual2],
-              ['B', + pianif2],
-              ['C', + progr2],
-              ['A - B', + attual2],
-              ['B - C', + pianif2],
-              ['A - C', + 0]
-          ]
-      }]
-    });
+//    var progr2  = parseFloat($(".soggetti-attuatori-2 .importo-soggetti-programmati").html());
+//    var pianif2 = parseFloat($(".soggetti-attuatori-2 .importo-soggetti-pianificati").html());
+//    var attual2 = parseFloat($(".soggetti-attuatori-2 .importo-soggetti-attuati").html());
+//
+//    $('#chart-venn-soggetti2').highcharts({
+//      chart:{
+//        backgroundColor: "rgba(255, 255, 255, 0)",
+//        margin: 0,
+//        padding: 0
+//      },
+//      title: {
+//          text: ''
+//      },
+//      tooltip: { enabled: false },
+//      plotOptions: {
+//          series: {
+//            showInLegend: false,
+//            //point: {
+//            //  events: {
+//            //    mouseOver: function(){
+//            //      this.graphic.element.setAttribute('stroke-width', 2);
+//            //      this.graphic.element.setAttribute('stroke', '#00f');
+//            //    },
+//            //    mouseOut: function () {
+//            //      this.graphic.element.setAttribute('stroke-width', 0);
+//            //    }
+//            //  }
+//            //}
+//          }
+//      },
+//      series: [{
+//          type: 'venn',
+//          data: [
+//              ['A', + attual2],
+//              ['B', + pianif2],
+//              ['C', + progr2],
+//              ['A - B', + attual2],
+//              ['B - C', + pianif2],
+//              ['A - C', + 0]
+//          ]
+//      }]
+//    });
     //return false;
     
     
     
-    
-    
-    var progr3  = parseFloat($(".soggetti-attuatori-3 .importo-soggetti-programmati").html());
-    var pianif3 = parseFloat($(".soggetti-attuatori-3 .importo-soggetti-pianificati").html());
-    var attual3 = parseFloat($(".soggetti-attuatori-3 .importo-soggetti-attuati").html());
+//
+//
+//    var progr3  = parseFloat($(".soggetti-attuatori-3 .importo-soggetti-programmati").html());
+//    var pianif3 = parseFloat($(".soggetti-attuatori-3 .importo-soggetti-pianificati").html());
+//    var attual3 = parseFloat($(".soggetti-attuatori-3 .importo-soggetti-attuati").html());
 
-    $('#chart-venn-soggetti3').highcharts({
-      chart:{
-        backgroundColor: "rgba(255, 255, 255, 0)",
-        margin: 0,
-        padding: 0
-      },
-      title: {
-          text: ''
-      },
-      tooltip: { enabled: false },
-      plotOptions: {
-          series: {
-            showInLegend: false,
-            //point: {
-            //  events: {
-            //    mouseOver: function(){
-            //      this.graphic.element.setAttribute('stroke-width', 2);
-            //      this.graphic.element.setAttribute('stroke', '#00f');
-            //    },
-            //    mouseOut: function () {
-            //      this.graphic.element.setAttribute('stroke-width', 0);
-            //    }
-            //  }
-            //}
-          }
-      },
-      series: [{
-          type: 'venn',
-          data: [
-              ['A', + attual3],
-              ['B', + pianif3],
-              ['C', + progr3],
-              ['A - B', + attual3],
-              ['B - C', + pianif3],
-              ['A - C', + 0]
-          ]
-      }]
-    });
+//    $('#chart-venn-soggetti3').highcharts({
+//      chart:{
+//        backgroundColor: "rgba(255, 255, 255, 0)",
+//        margin: 0,
+//        padding: 0
+//      },
+//      title: {
+//          text: ''
+//      },
+//      tooltip: { enabled: false },
+//      plotOptions: {
+//          series: {
+//            showInLegend: false,
+//            //point: {
+//            //  events: {
+//            //    mouseOver: function(){
+//            //      this.graphic.element.setAttribute('stroke-width', 2);
+//            //      this.graphic.element.setAttribute('stroke', '#00f');
+//            //    },
+//            //    mouseOut: function () {
+//            //      this.graphic.element.setAttribute('stroke-width', 0);
+//            //    }
+//            //  }
+//            //}
+//          }
+//      },
+//      series: [{
+//          type: 'venn',
+//          data: [
+//              ['A', + attual3],
+//              ['B', + pianif3],
+//              ['C', + progr3],
+//              ['A - B', + attual3],
+//              ['B - C', + pianif3],
+//              ['A - C', + 0]
+//          ]
+//      }]
+//    });
     //return false;
 
 
 
-})
+});
