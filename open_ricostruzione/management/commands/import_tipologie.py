6e570678ed942f42aa1767050fe7a98efcbcbfe9
verganis <stefano.vergani.it@gmail.com>
2015-10-12 09:33:18 +0200

    import tipologie changed encoding

--- open_ricostruzione/management/commands/import_tipologie.py
+++ open_ricostruzione/management/commands/import_tipologie.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
     )
 
     input_file = None
-    encoding = 'latin-1'
+    encoding = 'utf-8'
     logger = logging.getLogger('csvimport')
     codifiche = None
     anagrafiche = None
